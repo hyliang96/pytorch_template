b9a2a9068a8b259c6fcc2c276207684176a4f8c6
hyliang96 <hyliang96@hotmail.com>
2019-09-01 00:26:48 +0800

    remove commented codes in epoch.py

--- code/epoch.py
+++ code/epoch.py
@@ -34,51 +34,12 @@ class AverageValueMeter(meter.AverageValueMeter):
 
     def value(self):
         return super().value()[0]
-
     # 变量 std
-
 
 class ConfusionMeter(meter.ConfusionMeter):
     @detach_input
     def add(self, *args, **wargs):
         return super().add(*args, **wargs)
-
-# def get_value_std(f):
-#     @wraps(f)
-#     def _value(self):
-#         return f(self)[0]
-#     @wraps(f)
-#     def _std(self):
-#         return f(self)[1]
-#     return _value, _std
-
-# meter.meter.Meter.__call__, meter.meter.Meter.std  = get_value_std(meter.meter.Meter.value)
-
-
-# # def _value(self):
-# #     return self.value()[0]
-# # meter.meter.Meter.__call__ = _value
-
-# # def _std(self):
-# #     return self.value()[1]
-# # meter.meter.Meter.std = _std
-
-# def detach_input(f):
-#     @wraps(f)
-#     def _f(self, *args):
-#         args = [arg.detach() if type(arg)==torch.Tensor else arg for arg in args ]
-#         return f(self, *args)
-#     return _f
-# # meter.meter.Meter.add = detach_input(meter.meter.Meter.add)
-# meter.ClassErrorMeter.add = detach_input(meter.ClassErrorMeter.add)
-
-# def _add(self, *args):
-#     args = [arg.detach() if type(arg)==torch.Tensor else arg for arg in args ]
-#     return self.add(*args)
-# meter.ClassErrorMeter.add = _add
-
-# class Meter(object):
-
 
 
 def run_epoch(stage, state, data_loader):
@@ -124,60 +85,3 @@ def run_epoch(stage, state, data_loader):
     # print('_conf')
     # print(_conf.value())
 
-
-# def train(state, train_loader, stage='train'):
-#     state.model.train()
-
-#     pbar = tqdm(initial=0, total=len(train_loader), leave=False)
-#     loss_collector = AverageValueMeter()
-#     correct_collector = ClassErrorMeter(accuracy=True)
-# #     confusion_collector = ConfusionMeter()
-
-#     for batch_idx, (data, target) in enumerate(train_loader):
-#         data, target = data.to(state.args.device), target.to(state.args.device)
-#         state.optimizer.zero_grad()
-#         output = state.model(data)
-#         loss = F.nll_loss(output, target)
-#         loss.backward()
-#         state.optimizer.step()
-
-#         loss_collector.add(loss.mean().item())
-#         correct_collector.add(output, target)
-
-#         if batch_idx % state.args.log_interval == 0:
-#             pbar.desc = '{:6s} Epoch {}:'.format(stage, state.epoch)
-#             pbar.postfix = 'Loss {:.4f} Acc {:.4f}%'.format(
-#                 loss_collector.value(), correct_collector.value())
-#             pbar.update(state.args.log_interval)
-
-#     pbar.close()
-#     print('{:6s} Epoch {}: loss {:.4f}, Acc {:.4f}%'.format(
-#         stage, state.epoch, loss_collector.value(), correct_collector.value()))
-
-
-# def test(state, test_loader, stage='test'):
-#     state.model.eval()
-
-#     pbar = tqdm(initial=0, total=len(test_loader), leave=False)
-#     loss_collector = AverageValueMeter()
-#     correct_collector = ClassErrorMeter(accuracy=True)
-
-#     with torch.no_grad():
-#         for batch_idx, (data, target) in enumerate(test_loader):
-#             data, target = data.to(state.args.device), target.to(state.args.device)
-#             output = state.model(data)
-#             loss = F.nll_loss(output, target)
-
-#             loss_collector.add(loss.mean().item())# sum up batch loss
-#         #     pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
-#             correct_collector.add(output, target)
-
-#             if batch_idx % state.args.log_interval == 0:
-#                 pbar.desc = '{:6s} Epoch {}:'.format(stage, state.epoch)
-#                 pbar.postfix = 'Loss {:.4f} Acc {:.4f}%'.format(
-#                     loss_collector.value(), correct_collector.value())
-#                 pbar.update()
-
-#     pbar.close()
-#     print('{:6s} Epoch {}: loss {:.4f}, Acc {:.4f}%'.format(
-#         stage, state.epoch, loss_collector.value(), correct_collector.value()))
