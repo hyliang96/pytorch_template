26827c5cbcf4477e68546ac01e3a8e362f159cba
hyliang96 <hyliang96@hotmail.com>
2019-09-04 02:46:48 +0800

    experiemnt

--- code/run.sh
+++ code/run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-
-git add -A ../ && \
-git commit -m "experiment" && \
-git tag -a "$1" -m "experiment" && \
-python3 main.py --exper "$1"
+if [ "$1" = 'help' ] || [ "$1" = '--help' ] || [ "$1" = '-h' ]; then
+    echo "Usage:"
+    echo "gpuid [n,m,...] run.sh [experiment_name]"
+    echo "gpuid [n,m,...] run.sh --rerun [experiment_name]"
+elif [ "$1" = '--rerun' ]; then
+    python3 main.py --exper "$2"
+else
+    git add -A ../ && \
+    git commit -m "experiment" && \
+    git tag -a "$1" -m "experiment" && \
+    python3 main.py --exper "$1"
+fi
