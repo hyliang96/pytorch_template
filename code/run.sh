d37bc6619d49c455a49364379771769627978e3c
hyliang96 <hyliang96@hotmail.com>
2019-09-04 02:54:07 +0800

    add run.sh in code/ to automatically commit or check by tag with experiment's name

--- code/run.sh
+++ code/run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+
 if [ "$1" = 'help' ] || [ "$1" = '--help' ] || [ "$1" = '-h' ]; then
     echo "Usage:"
     echo "gpuid [n,m,...] run.sh [experiment_name]         : commit a new experiment and tag it with its name"
-    echo "gpuid [n,m,...] run.sh --rerun [experiment_name] : git checkout old tag and rerun the experiment"
-elif [ "$1" = '--rerun' ]; then
+    echo "gpuid [n,m,...] run.sh --rerun(-r) [experiment_name] : git checkout old tag and rerun the experiment"
+elif [ "$1" = '--rerun' ] || [ "$1" = '-r' ]; then
     git checkout "$2" && \
     python3 main.py --exper "$2"
 else
