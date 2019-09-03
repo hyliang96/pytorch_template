4780f9ff686e1bb03275f2913b0fad9d25d907ae
hyliang96 <hyliang96@hotmail.com>
2019-09-04 02:41:30 +0800

    experiment

--- code/run.sh
+++ code/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git add -A ../
-git commit -m "experiment"
+git add -A ../ && \
+git commit -m "experiment" && \
 git tag -a "$1" -m "experiment" && \
 python3 main.py --exper "$1"
