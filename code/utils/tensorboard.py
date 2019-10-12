3bf103fe49cb2de1af71804295b2a485a994f30c
hyliang96 <hyliang96@hotmail.com>
2019-10-12 23:43:17 +0800

    "update to pytorch 1.3.0 and tensorfllow 2.0; close future warning when importing tensorboard; waiting for more testing"

--- code/utils/tensorboard.py
+++ code/utils/tensorboard.py
@@ -1,4 +1,12 @@
+# tensorboard<2.0 加载时会报如下警告，其余tensorboard>=2.0不会
+    # /home/haoyu/ENV/localENV/anaconda3/lib/python3.7/site-packages/tensorboard/compat/tensorflow_stub/dtypes.py:541:
+    # FutureWarning: Passing (type, 1) or '1type' as a synonym of type is deprecated; in a future version of numpy,
+    # it will be understood as (type, (1,)) / '(1,)type'.
+# 如此以在加载tensorboard时暂时关闭之
+__import__('warnings').filterwarnings('ignore',category=FutureWarning)
 from torch.utils.tensorboard.writer import SummaryWriter
+__import__('warnings').filterwarnings('default',category=FutureWarning)
+
 import threading
 import os, sys, signal
 
